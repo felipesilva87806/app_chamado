4fed147f7894361f2729ab2ed3306c25f9366c2e
Felipe Silva <fsilva87806@gmail.com>
2020-09-02 18:16:10 -0300

    Configuração dos combobox loja e criação das tabelas no banco

--- App_Chamados/Views/frm_AberturaDeChamados.cs
+++ App_Chamados/Views/frm_AberturaDeChamados.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using App_Chamados.Entidades;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,28 @@ namespace App_Chamados.Views {
     public partial class frm_AberturaDeChamados : Form {
         public frm_AberturaDeChamados() {
             InitializeComponent();
+        }
+
+        private void frm_AberturaDeChamados_Load(object sender, EventArgs e) {
+            MySqlCommand cmd = new MySqlCommand();
+            ConexaoBd conn = new ConexaoBd();
+
+            cmd.CommandText = "select nome_loja as LOJAS from lojas order by id_loja";
+            cmd.Connection = conn.Conectar();
+
+            try {
+                MySqlDataReader dr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                comboBox_loja.DisplayMember = "LOJAS";
+                comboBox_loja.DataSource = dt;
+            }
+            catch (MySqlException ex) {
+                MessageBox.Show("Ocorreu um erro: " + ex.ToString());
+            }
+            finally {
+                conn.desconectar();
+            }
         }
     }
 }
